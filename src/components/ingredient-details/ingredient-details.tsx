13ceffe01276b92975fc690285e37638a2f1edcb
Ivan Lednev <feyrune33@yandex.ru>
2026-01-17 00:39:07 +0300

    fix: handle direct navigation to /ingredients/:id
    - Check if ingredients are loaded in IngredientDetails
    - If not  dispatch fetchIngredients()
    - Show Preloader during loading
    - Display error if ingredient not found
    - Now works on direct page access and modal
    
    Fixes infinite loader on direct ingredient page open.

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,24 +1,40 @@
 import { FC, useEffect } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
-import { useSelector } from '../../services/store';
-import { selectIngredients } from '../../slices/ingredientsSlice';
+import { useSelector, useDispatch } from '../../services/store';
+import {
+  selectIngredients,
+  selectLoading as selectIngredientsLoading
+} from '../../slices/ingredientsSlice';
 import { useLocation, useParams } from 'react-router-dom';
+import { fetchIngredients } from '../../slices/ingredientsSlice';
 import { TIngredient } from '@utils-types';
 
 export const IngredientDetails: FC = () => {
   const { id } = useParams<{ id: string }>();
   const location = useLocation();
-  const ingredients = useSelector(selectIngredients);
+  const dispatch = useDispatch();
 
-  const backgroundLocation = location.state?.background?.location;
+  const ingredients = useSelector(selectIngredients);
+  const isLoading = useSelector(selectIngredientsLoading);
+
   const ingredientData: TIngredient | undefined = ingredients.find(
     (item) => item._id === id
   );
 
-  if (!ingredients.length || !ingredientData) {
+  useEffect(() => {
+    if (!ingredients.length && !isLoading) {
+      dispatch(fetchIngredients());
+    }
+  }, [dispatch, ingredients.length, isLoading]);
+
+  if (isLoading || (!ingredientData && ingredients.length > 0)) {
     return <Preloader />;
   }
 
-  return <IngredientDetailsUI ingredientData={ingredientData} />;
+  if (ingredientData) {
+    return <IngredientDetailsUI ingredientData={ingredientData} />;
+  }
+
+  return <div className='text text_type_main-large'>Ингредиент не найден</div>;
 };
